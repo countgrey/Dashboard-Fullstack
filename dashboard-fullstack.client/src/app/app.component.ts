b2e08e30639c33c1a0b943a008099bbd87bd439a
countgrey <mag.34737@gmail.com>
2024-03-08 10:07:29 +0500

    start

--- dashboard-fullstack.client/src/app/app.component.ts
+++ dashboard-fullstack.client/src/app/app.component.ts
@@ -1,21 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-  public forecasts: WeatherForecast[] = [];
-
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
@@ -23,14 +14,7 @@ export class AppComponent implements OnInit {
   }
 
   getForecasts() {
-    this.http.get<WeatherForecast[]>('/weatherforecast').subscribe(
-      (result) => {
-        this.forecasts = result;
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
+    
   }
 
   title = 'dashboard-fullstack.client';
